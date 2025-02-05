ff11484e8396c1f5e81461a03c2ba6f9bfa98b36
SamuelE0271194 <e0271194@u.nus.edu>
2025-02-05 14:46:43 +0100

    removed debug lines

--- TD4/x25519.py
+++ TD4/x25519.py
@@ -8,19 +8,19 @@ def main():
     #first argument = m, second argument = u
 
     m = args[0]
-    print(m)
+    #print(m)
     m = Montgomery.split_to_numbers(m)
     m = Montgomery.decodeScalar25519(m)
     
     u = 9
     if (len(args) > 1):
         u = args[1]
-        print(u)
+        #print(u)
         u = Montgomery.split_to_numbers(u)
         u = Montgomery.decodeUCoordinate(u)
-    print("~~~~~~~~")
     out = Montgomery.ladder(m, Montgomery.Point(u, 1))
-    print(out)
+    #print("~~~~~~~~")
+    #print(out)
     print(Montgomery.encodeUCoordinate(out))
 
     return
